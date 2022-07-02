cfc4f05624fc4ca122d2ec78e0afcc743a6c17a2
SirTonakton <vagma1985@gmail.com>
2022-07-02 14:38:22 -0700

    first commit

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,8 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
 
+#Add our dependencies.
+
 import csv
 
 import os
@@ -15,18 +17,36 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign a variable to save the file to a path
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#1. Initialize a total vote counter.
+total_votes = 0
 
+#New List
+candidate_options = []
 
 #Open the election results and read th file
 with open(file_to_load) as election_data:
-
-    #To do: read and analyze the data here.
-
-    #Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    #Print the header row.
+    #Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    #Print each row in the CSV file.
+    for row in file_reader:
+        
+        #2. Add to the total vote count.
+        total_votes += 1
+
+        #Print the candidate name for each row.
+        candidate_name = row[2]
+
+        #If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            
+            #Add it to the lst of candidates
+            candidate_options.append(candidate_name)
+        
+#3. Print the total votes.
+print(candidate_options)
+
     
 
